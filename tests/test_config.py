3e84257817b04b08cc55aae50aa5ed97fc7abc35
unknown <lilacrapture@gmail.com>
2020-02-25 23:10:54 +0300

    Add config test

---
+++ tests/test_config.py
@@ -0,0 +1,10 @@
+from src.config import tokens_by_type
+import unittest
+
+class TestConfig(unittest.TestCase):
+
+    def test_tokens_by_type(self):
+        dict = {')': {'type': "paren"},
+                '+': {'type': "operator"}}
+        result = tokens_by_type(dict, 'operator')
+        self.assertEqual(result, ['+'])

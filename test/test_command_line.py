67cd0ba644f9380d07b181d7600cea2ae04af9d6
Nataly <lilacrapture@gmail.com>
2020-02-27 19:52:56 +0300

    Add command_line test

---
+++ test/test_command_line.py
@@ -0,0 +1,9 @@
+import calculator.command_line as command_line
+import unittest
+
+class TestCommandLine(unittest.TestCase):
+
+    def test_process_args(self):
+        args_list = ["module_name", '1', '*', '3.5', '/', '.5']
+        result = command_line.process_args(args_list)
+        self.assertEqual(result, 7.0)

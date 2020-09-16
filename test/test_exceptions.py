dfb4d2a2f9bd7fd4a4c357bb2f5f1bd8e2c9a547
LilacRapture <39832289+LilacRapture@users.noreply.github.com>
2020-09-17 00:38:03 +0300

    Add test of regular function

--- test/test_exceptions.py
+++ test/test_exceptions.py
@@ -5,10 +5,17 @@ import unittest
 def excepting_function():
     raise CalcException(2, ['1', '+', '2'], message='message')
 
+def regular_function():
+    return 'calc'
+
 class TestExceptions(unittest.TestCase):
 
     def test_catch_calc_exceptions(self):
         result = exceptions.catch_calc_errors(excepting_function)
         expected_location = '1 + 2\n    ^'
-        print('\n' + expected_location)
         self.assertEqual(expected_location, result['error_location'])
+
+    def test_catch_calc_exceptions_regular(self):
+        result = exceptions.catch_calc_errors(regular_function)
+        self.assertEqual('success', result['status'])
+        self.assertEqual('calc', result['result'])
